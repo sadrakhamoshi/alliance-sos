fbd61a4825b67ae6651926c8f7f46a26f287663c
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2020-08-20 18:57:28 +0430

    impelement basic Auth

--- Alliance-SOS/app/src/main/java/com/example/alliancesos/MainActivity.java
+++ Alliance-SOS/app/src/main/java/com/example/alliancesos/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.alliancesos;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mUsername;
+    private static final String ANONYMOUS = "anonymous";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
