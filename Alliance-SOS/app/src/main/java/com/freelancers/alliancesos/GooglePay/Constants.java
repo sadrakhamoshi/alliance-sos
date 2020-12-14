c235b66315786d41ca211ee2f4501e27be437a7c
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2020-12-14 16:07:16 +0330

    nothing

--- Alliance-SOS/app/src/main/java/com/freelancers/alliancesos/GooglePay/Constants.java
+++ Alliance-SOS/app/src/main/java/com/freelancers/alliancesos/GooglePay/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
      *
      * @value #PAYMENTS_ENVIRONMENT
      */
-    public static final int PAYMENTS_ENVIRONMENT = WalletConstants.ENVIRONMENT_TEST;
+    public static final int PAYMENTS_ENVIRONMENT = WalletConstants.ENVIRONMENT_PRODUCTION;
 
     /**
      * The allowed networks to be requested from the API. If the user has cards from networks not
