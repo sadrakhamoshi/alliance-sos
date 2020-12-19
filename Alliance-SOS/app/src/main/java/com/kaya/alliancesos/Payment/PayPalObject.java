4d2d6adadced12f1db248b958a5fd94aed03d0ba
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2020-12-19 15:01:47 +0330

    nothing

--- Alliance-SOS/app/src/main/java/com/kaya/alliancesos/Payment/PayPalObject.java
+++ Alliance-SOS/app/src/main/java/com/kaya/alliancesos/Payment/PayPalObject.java
@@ -5,9 +5,9 @@ import com.paypal.android.sdk.payments.PayPalConfiguration;
 //AeQK2pqMHePzuUWGatHyrNeLVzRcTAi3ysTK82Sf0tYC3usBxUjDzqU4s-fV4OCYGkZ02oMyx2iKqze0 production
 //AWwyx4t0OVMd83fjJS_RYOO4lEMsfFjzGVwLWjwGl-5mVj_CRGouIRQiVnBxTC8MKKVYXcoqlYm9-lHP test
 public class PayPalObject {
-    private static final String CLIENT_ID = "AeQK2pqMHePzuUWGatHyrNeLVzRcTAi3ysTK82Sf0tYC3usBxUjDzqU4s-fV4OCYGkZ02oMyx2iKqze0";
+    private static final String CLIENT_ID = "AWwyx4t0OVMd83fjJS_RYOO4lEMsfFjzGVwLWjwGl-5mVj_CRGouIRQiVnBxTC8MKKVYXcoqlYm9-lHP";
     public static PayPalConfiguration config = new PayPalConfiguration()
 
-            .environment(PayPalConfiguration.ENVIRONMENT_PRODUCTION)
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
             .clientId(CLIENT_ID);
 }
